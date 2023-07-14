1b5ef9f83f6374b5b78e96123b46a209b7f55fdd
fromant <fromant65@gmail.com>
2023-07-14 09:59:07 -0300

    removed test files and modules

--- src/__mocks__/firebase.js
+++
@@ -1,7 +0,0 @@
-//These constants mock the firebase client functionality
-
-export const signInWithEmailAndPassword = jest
-  .fn()
-  .mockResolvedValue({ user: { uid: "testUserId" } });
-export const signOut = jest.fn().mockResolvedValue();
-// Add other Firebase methods you're using in your app
--- src/setupTests.js
+++
@@ -1,4 +0,0 @@
-import "@testing-library/jest-dom";
-import "firebase-admin"; // Import firebase-admin to mock Firebase services
-
-//Make tests
